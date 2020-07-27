72da1fee98e5fd2ee999737fa61bdf6cb20d508f
Georges Duverger <georges.duverger@gmail.com>
2020-07-27 15:12:33 -0400

    0.1.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='crono-api-client',
-	version='0.1.12',
+	version='0.1.14',
 	author='Georges Duverger',
 	author_email='georges.duverger@gmail.com',
 	description='Crono API client',
